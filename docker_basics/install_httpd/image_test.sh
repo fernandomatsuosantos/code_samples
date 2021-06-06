d83f095abb79d125d3677d06804af1c8d0eb401b
Fernando Matsuo Santos <fernando.matsuo@moduscreate.com>
2021-06-05 22:04:47 -0300

    Add docker PoC

--- docker_basics/install_httpd/image_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#-e if script line fails it fails entire script
-#-o will make if any command in the pipeline fails
-set -eo pipefail
-
-#Enable BASH debug mode = You will see actual commands being executed with all variables applied
-[ "$DEBUG" ] && set -x
